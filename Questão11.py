443f46504ebcc09360b1a739d8a6b269c07952c8
Eimilly__S2 <emillysantosaaraujo@gmail.com>
2026-03-03 18:47:54 -0300

    Add files via upload
    11.              Receba o raio de uma circunferência. Calcule e mostre o comprimento da circunferência.

---
+++ Questão11.py
@@ -0,0 +1,12 @@
+#Declarar
+
+Raio : int = 0 
+Comprimento : int = 0
+
+#Inicio
+
+Raio = int (input("Digite o valor do Raio de uma circuferência: "))
+Comprimento = Raio * 2 * 3.14
+print("O comprimento é: ",Comprimento)
+
+#Fim
